c09fb199fc0021a85883890388299bc6f54060f4
wbeatty <wbeatty@protonmail.com>
2025-05-20 20:29:17 -0500

    Fixed a bug where datasets wouldn't be displayed properly

--- app.py
+++ app.py
@@ -16,9 +16,13 @@ def index():
 
 @app.route("/get_graph_data")
 def get_graph_data():
-    csv_path = os.path.join(app.static_folder, "data", "AMZN_filtered.csv")
+    csv_path = os.path.join(app.static_folder, "data", "graph_data.csv")
     df = pd.read_csv(csv_path)
-    data = {"Date": df["date"].tolist(), "Adj Close": df["adj_close"].tolist()}
+    data = {
+        "Date": df["date"].tolist(),
+        "AMZN Close": df["AMZN_adj_close"].tolist(),
+        "SPX Close": df["SPX_adj_close"].tolist(),
+    }
     return jsonify(data)
 
 
